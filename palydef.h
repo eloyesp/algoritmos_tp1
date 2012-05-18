1a397d8f7020233ec2abedd941f1ac3024227358
Daiana Giorgi <daia_gior@hotmail.com>
2012-05-18 15:19:50 -0300

    completo palydef

--- palydef.h
+++ palydef.h
@@ -4,11 +4,172 @@
 #include <string.h>
 char pal[50][5], def[50][40];
 
-void cargar_diccionario(void) {
+void cargar_diccionario(void)
+{
 	strcpy(pal[0], "oso");
 	strcpy(def[0], "Animal muy grande que te abraza.");
 
+	strcpy(pal[0], "une");
+	strcpy(def[0], "Junta, enlaza una cosa con otra.");
+
+	strcpy(pal[0], "ron");
+	strcpy(def[0], "Licor alcohólico de la melaza.");
+
+	strcpy(pal[0], "log");
+	strcpy(def[0], "Logaritmo.");
+
+	strcpy(pal[0], "tnt");
+	strcpy(def[0], "Explosivo poderoso.");
+
+	strcpy(pal[0], "aji");
+	strcpy(def[0], "Pimiento.");
+
+	strcpy(pal[0], "ley");
+	strcpy(def[0], "Precepto dictado por la suprema autoridad.");
+
+	strcpy(pal[0], "feo");
+	strcpy(def[0], "Que carece de belleza.");
+
+	strcpy(pal[0], "uno");
+	strcpy(def[0], "Numero que le sigue al cero.");
+
+	strcpy(pal[0], "sol");
+	strcpy(def[0], "Estrella luminosa cercana a la tierra.");
+
+	strcpy(pal[0], "ojo");
+	strcpy(def[0], "Organo de la vista OJO");
+
+	strcpy(pal[0], "ola");
+	strcpy(def[0], "Movimiento que hace el agua en el mar.");
+
+	strcpy(pal[0], "sal");
+	strcpy(def[0], "Cloruro de sodio.");
+
+	strcpy(pal[0], "rap");
+	strcpy(def[0], "Ritmo negro urbano.");
+
+	strcpy(pal[0], "uva");
+	strcpy(def[0], "Fruta pequeña y redonda.");
+
+	strcpy(pal[0], "oro");
+	strcpy(def[0], "Metal amarillo.");
+
+	strcpy(pal[0], "oca");
+	strcpy(def[0], "El juego de la...");
+
+	strcpy(pal[0], "via");
+	strcpy(def[0], "Por donde pasa el tren.");
+
+	strcpy(pal[0], "cal");
+	strcpy(def[0], "CaO");
+
+	strcpy(pal[0], "pan");
+	strcpy(def[0], "Alimento (en general)");
+
+	strcpy(pal[0], "ARN");
+	strcpy(def[0], "Ácido ribonucleico");
+
+	strcpy(pal[0], "tes");
+	strcpy(def[0], "Rostro");
+
 	strcpy(pal[20], "casa");
 	strcpy(def[20], "Habitat de un ser humano.");
+
+	strcpy(pal[20], "adios");
+	strcpy(def[20],"chau.");
+
+	strcpy(pal[20],"soda");
+	strcpy(def[20],"Agua con gas.");
+
+	strcpy(pal[20],"sopa");
+	strcpy(def[20],"Alimento liquido.");
+
+	strcpy(pal[20],"joya");
+	strcpy(def[20],"Alahja.");
+
+	strcpy(pal[20],"gota");
+	strcpy(def[20],"Cantidad muy pequeña de liquido.");
+
+
+	strcpy(pal[20],"pena");
+	strcpy(def[20],"Castigo.");
+
+
+	strcpy(pal[20],"chip");
+	strcpy(def[20],"Circuito integrado electronico.");
+
+
+	strcpy(pal[20],"lila");
+	strcpy(def[20],"Violeta claro.");
+
+
+	strcpy(pal[20],"tour");
+	strcpy(def[20],"Excursion turistica.");
+
+
+	strcpy(pal[20], "alfa");
+	strcpy(def[20],"letra griega a.");
+
+
+	strcpy(pal[20],"zeus");
+	strcpy(def[20],"Dios del olimpo.");
+
+
+	strcpy(pal[20],"auto");
+	strcpy(def[20],"Medio de transporte familiar.");
+
+	strcpy(pal[20],"Arpa");
+	strcpy(def[20],"Instrumento musical de cuerdas.");
+
+
+	strcpy(pal[20],"tuba");
+	strcpy(def[20],"Instrumento mucical de viento.");
+
+
+	strcpy(pal[20],"papa");
+	strcpy(def[20],"Tuberculo.");
+
+	strcpy(pal[20],"coco");
+	strcpy(def[20],"Fruta de palmera.");
+
+	strcpy(pal[20],"rata");
+	strcpy(def[20],"Mamifero roedor pequeño.");
+
+	strcpy(pal[20],"cana");
+	strcpy(def[20],"Pelo blanco.");
+
+	strcpy(pal[20],"nono");
+	strcpy(def[20],"Dos veces no.");
+
+	strcpy(pal[20],"ogro");
+	strcpy(def[20],"criatura verde animada que es mala");
+
+	strcpy(pal[20],"jefe");
+	strcpy(def[20],"Persona que tiene poder o autoridad sobre un grupo para dirigir su trabajo.");
+
+	strcpy(pal[20],"gato");
+	strcpy(def[20],"Animal felino.");
+
+	strcpy(pal[20],"cama");
+	strcpy(def[20],"Lugar de descanso.");
+
+	strcpy(pal[20],"boca");
+	strcpy(def[20],"Orificio donde entra la comida.");
+
+	strcpy(pal[20],"loco");
+	strcpy(def[20],"Persona fuera de si.");
+
+	strcpy(pal[20],"ropa");
+	strcpy(def[20],"Cualquier prenda de tela que sirve para vestir.");
+
+	strcpy(pal[20],"foco");
+	strcpy(def[20],"Lampara que emite luz.");
+
+	strcpy(pal[20],"mayo");
+	strcpy(def[20],"Quinto mes del año.");
+
+	strcpy(pal[20],"taza");
+	strcpy(def[20],"Vasija pequeña para tomar liquidos.");
+
 }
 
