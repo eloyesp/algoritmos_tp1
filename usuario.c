9e82d97a72b57b5c2c977d96a333195cf6325e04
Eloy Espinaco <eloyesp@gmail.com>
2012-07-23 11:39:37 -0300

    solicitudes más completas

--- usuario.c
+++ usuario.c
@@ -1,35 +1,51 @@
 #include "usuario.h"
+#include "utils.h"
 #include <stdio.h>
 #include <string.h>
 
 int usuario_alta(void){
 	FILE * usuarios;
 	Usuario nuevo_usuario;
-	char input, * profesiones[] = { "contador", "abogado", "ingeniero"};
+	char input[MAX_NOMBRE], * profesiones[] = { "contador", "abogado", "ingeniero"};
 	
-	usuarios = fopen("usuarios.dat", "w+");
+	usuarios = fopen("usuarios.dat", "wb+");
 	
 	printf("Ingrese la profesion: ");
-	gets(input);
+	read_line(input, MAX_NOMBRE);
+	strcpy(input, nuevo_usuario.profesion);
 	
 	if (profesion_valida()) {
 		if ( usuarios == NULL)
 			printf("No se pudo abrir el archivo.");
 		else {
-			printf("Ingrese Nombre, Apellido, DNI, Fecha de nacimiento, Fecha de ingreso al Colegio");
-			fscanf(stdin, "%s,%s,%d,%d,%d,%d,%d,%d,%d,%s",
-				nuevo_usuario.nombre,
-				nuevo_usuario.apellido,
-				&nuevo_usuario.dni,
-				&nuevo_usuario.nacimiento.dia,
-				&nuevo_usuario.nacimiento.mes,
-				&nuevo_usuario.nacimiento.anio,
-				&nuevo_usuario.inscripcion.dia,
-				&nuevo_usuario.inscripcion.mes,
-				&nuevo_usuario.inscripcion.anio);
-			strcpy(input, nuevo_usuario.profesion);
-			fseek(usuarios, (nuevo_usuario.dni - 1) * sizeof(Usuario), SEEK_SET);
+			printf("Ingrese Nombre: ");
+			read_line(nuevo_usuario.nombre, MAX_NOMBRE);
+			printf("Apellido: ");
+			read_line(nuevo_usuario.apellido, MAX_NOMBRE);
+			printf("DNI: ");
+			scanf("%d",&nuevo_usuario.dni);
+			printf("Fecha de nacimiento:\n");
+			printf("Dia: ");
+			scanf("%d",&nuevo_usuario.nacimiento.dia);
+			printf("Mes: ");
+			scanf("%d",&nuevo_usuario.nacimiento.mes);
+			printf("Anio: ");
+			scanf("%d",&nuevo_usuario.nacimiento.anio);
+			printf("Fecha de ingreso al colegio:\n");
+			printf("Dia: ");
+			scanf("%d",&nuevo_usuario.inscripcion.dia);
+			printf("Mes: ");
+			scanf("%d",&nuevo_usuario.inscripcion.mes);
+			printf("Anio: ");
+			scanf("%d",&nuevo_usuario.inscripcion.anio);
+			nuevo_usuario.num_usuario = 0;	
+			
+			fseek(usuarios, (nuevo_usuario.num_usuario) * sizeof(Usuario), SEEK_SET);
 			fwrite(&nuevo_usuario, sizeof(Usuario), 1, usuarios);
+			printf("Enhorabuena, ha sido registrado: %s, %i",
+				nuevo_usuario.nombre,
+				nuevo_usuario.num_usuario);
+			//system("pause");
 		}	
 		fclose(usuarios);
 		return 1;
@@ -49,6 +65,7 @@ int  usuario_login(void){
 }
 
 int profesion_valida() {
+	
 	return 1;
 	// !strcmp(alta.profesion,profesion1)
 }
